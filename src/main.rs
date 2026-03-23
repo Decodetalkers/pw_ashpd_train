f877a666408ed2dee4fb9962ef7bed8d1957da42
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-03-23 16:56:35 +0900

    feat: finish tee

--- src/main.rs
+++ src/main.rs
@@ -48,24 +48,46 @@ async fn main() -> ashpd::Result<()> {
     let stream_raw_fd = &stream_fd.as_raw_fd();
 
     let pipewire_source = gst::ElementFactory::make("pipewiresrc")
+        .name("pw")
         .property("fd", stream_raw_fd)
         .property("path", pipewire_node_id.to_string())
         .build()
         .unwrap();
 
-    let videoconvert = gst::ElementFactory::make("videoconvert").build().unwrap();
-    let videoconvert2 = gst::ElementFactory::make("videoconvert").build().unwrap();
-    let x264enc = gst::ElementFactory::make("x264enc").build().unwrap();
+    let videoconvert = gst::ElementFactory::make("videoconvert")
+        .name("videoconvert1")
+        .build()
+        .unwrap();
+    let videoconvert2 = gst::ElementFactory::make("videoconvert")
+        .name("videoconvert2")
+        .build()
+        .unwrap();
+    let x264enc = gst::ElementFactory::make("x264enc")
+        .property_from_str("tune", "zerolatency")
+        .build()
+        .unwrap();
     let flvmux = gst::ElementFactory::make("flvmux").build().unwrap();
     let filesink = gst::ElementFactory::make("filesink")
         .property("location", "xyz.flv")
         .build()
         .unwrap();
     let wayland_sink = gst::ElementFactory::make("waylandsink").build().unwrap();
-    let queue1 = gst::ElementFactory::make("queue").build().unwrap();
-    let queue2 = gst::ElementFactory::make("queue").build().unwrap();
+    let queue1 = gst::ElementFactory::make("queue")
+        .name("queue1")
+        .property("max-size-buffers", 50_u32)
+        .property("max-size-bytes", 0_u32)
+        .property("max-size-time", 0_u64)
+        .build()
+        .unwrap();
+    let queue2 = gst::ElementFactory::make("queue")
+        .name("queue2")
+        .property("max-size-buffers", 50_u32)
+        .property("max-size-bytes", 0_u32)
+        .property("max-size-time", 0_u64)
+        .build()
+        .unwrap();
     let tee = gst::ElementFactory::make("tee")
-        .property("name", "t")
+        .name("tee")
         .build()
         .unwrap();
     let pipeline = gst::Pipeline::default();
@@ -84,16 +106,18 @@ async fn main() -> ashpd::Result<()> {
         ])
         .unwrap();
     gst::Element::link_many([&pipewire_source, &tee]).unwrap();
-    gst::Element::link_many([&queue1, &videoconvert, &x264enc, &flvmux, &filesink]).unwrap();
-    gst::Element::link_many([&queue2, &videoconvert2, &wayland_sink]).unwrap();
+    gst::Element::link_many([&tee, &queue1, &videoconvert, &x264enc, &flvmux, &filesink]).unwrap();
+    gst::Element::link_many([&tee, &queue2, &videoconvert2, &wayland_sink]).unwrap();
 
-    let tee_pad_1 = tee.request_pad_simple("src_%u").unwrap();
-    let queue1_pad = queue1.static_pad("sink").unwrap();
-    let tee_pad_2 = tee.request_pad_simple("src_%u").unwrap();
-    let queue2_pad = queue2.static_pad("sink").unwrap();
-    tee_pad_1.link(&queue1_pad).unwrap();
-    tee_pad_2.link(&queue2_pad).unwrap();
-    pipeline.set_state(gst::State::Playing).unwrap();
+    //let tee_pad_1 = tee.request_pad_simple("src_%u").unwrap();
+    //let queue1_pad = queue1.static_pad("sink").unwrap();
+    //let tee_pad_2 = tee.request_pad_simple("src_%u").unwrap();
+    //let queue2_pad = queue2.static_pad("sink").unwrap();
+    //tee_pad_1.link(&queue1_pad).unwrap();
+    //tee_pad_2.link(&queue2_pad).unwrap();
+    //drop(queue2_pad);
+    //drop(queue1_pad);
+
     let pipeline_2 = pipeline.clone();
     let mut signals = Signals::new([SIGINT]).unwrap();
     let handle = std::thread::spawn(move || {
@@ -105,6 +129,7 @@ async fn main() -> ashpd::Result<()> {
         }
     });
 
+    pipeline.set_state(gst::State::Playing).unwrap();
     let bus = pipeline.bus().unwrap();
 
     for msg in bus.iter_timed(gst::ClockTime::NONE) {
